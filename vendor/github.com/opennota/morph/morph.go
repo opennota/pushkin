2e94bfad882fc4e2074aee9b37d25b252a7ec067
opennota <opennota@gmail.com>
2017-11-11 10:16:19 +0700

    Bundle dictionary data

--- vendor/github.com/opennota/morph/morph.go
+++ vendor/github.com/opennota/morph/morph.go
@@ -11,6 +11,8 @@
 // You should have received a copy of the GNU General Public License along
 // with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+//go:generate esc -o data.go -pkg morph data.gob
+
 // Package morph provides a simple morphological analyzer for Russian language,
 // using the compiled dictionaries from pymorphy2.
 package morph
@@ -18,10 +20,7 @@ package morph
 import (
 	"bytes"
 	"encoding/binary"
-	"encoding/json"
-	"os"
-	"os/exec"
-	"path/filepath"
+	"encoding/gob"
 	"strings"
 )
 
@@ -64,96 +63,23 @@ func Parse(word string) (words []string, norms []string, tags []string) {
 }
 
 func init() {
-	dir := dataPath()
-	prefixesPath := filepath.Join(dir, "paradigm-prefixes.json")
-	suffixesPath := filepath.Join(dir, "suffixes.json")
-	tagsPath := filepath.Join(dir, "gramtab-opencorpora-int.json")
-	paradigmsPath := filepath.Join(dir, "paradigms.array")
-	dawgPath := filepath.Join(dir, "words.dawg")
-
-	var err error
-
-	tags, err = loadStringArray(tagsPath)
-	if err != nil {
+	data, _ := FSByte(false, "/data.gob")
+	if err := gob.NewDecoder(bytes.NewReader(data)).Decode(
+		&struct {
+			Prefixes  *[]string
+			Suffixes  *[]string
+			Tags      *[]string
+			Paradigms *[][]uint16
+			DAWG      **dawg
+		}{
+			&prefixes,
+			&suffixes,
+			&tags,
+			&paradigms,
+			&d,
+		}); err != nil {
 		panic(err)
 	}
-
-	prefixes, err = loadStringArray(prefixesPath)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			panic(err)
-		}
-		prefixes = []string{"", "по", "наи"}
-	}
-
-	suffixes, err = loadStringArray(suffixesPath)
-	if err != nil {
-		panic(err)
-	}
-
-	if err := loadParadigms(paradigmsPath); err != nil {
-		panic(err)
-	}
-
-	d, err = newDAWG(dawgPath)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func dataPath() string {
-	cmd := exec.Command("python", "-c", "import pymorphy2_dicts_ru as p; print(p.__path__[0])")
-	var buf bytes.Buffer
-	cmd.Stdout = &buf
-	if err := cmd.Run(); err != nil {
-		panic("pymorphy2_dicts_ru is not installed")
-	}
-	dir := strings.TrimRight(buf.String(), "\r\n")
-	return filepath.Join(dir, "data")
-}
-
-func loadStringArray(fn string) ([]string, error) {
-	f, err := os.Open(fn)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	var ss []string
-	if err := json.NewDecoder(f).Decode(&ss); err != nil {
-		return nil, err
-	}
-	return ss, nil
-}
-
-func loadParadigms(fn string) error {
-	f, err := os.Open(fn)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	var paraCount uint16
-	if err := binary.Read(f, binary.LittleEndian, &paraCount); err != nil {
-		return err
-	}
-
-	paradigms = make([][]uint16, 0, paraCount)
-	for i := 0; i < int(paraCount); i++ {
-		var paraLen uint16
-		if err := binary.Read(f, binary.LittleEndian, &paraLen); err != nil {
-			return err
-		}
-
-		para := make([]uint16, paraLen)
-		if err := binary.Read(f, binary.LittleEndian, &para); err != nil {
-			return err
-		}
-
-		paradigms = append(paradigms, para)
-	}
-
-	return nil
 }
 
 func prefixSuffixTag(para []uint16, i int) (string, string, string) {
